91f8af1fa8361b4365d258b1cccdd3f11348d817
AKSHAY <akshayjadhav461999@gmail.com>
2020-11-03 13:43:50 +0530

    takePicture method added

--- App.js
+++ App.js
@@ -1,7 +1,32 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  Button,
+  ActivityIndicator,
+  StyleSheet,
+} from 'react-native';
+import {RNCamera} from 'react-native-camera';
 
+const LoaderView = () => {
+  <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <ActivityIndicator />
+  </View>;
+};
 export default function App() {
+  const [image, setImage] = useState(null);
+
+  const takePicture = async (camera) => {
+    try {
+      const options = {quality: 0.9, base64: false};
+      const data = await camera.takePictureAsync(options);
+      setImage(data.uri);
+    } catch (error) {
+      console.warn(error);
+    }
+  };
   return (
     <View>
       <Text>Camera App</Text>
